79d3e3018e676ded3c779521a7e35e11939cd0f0
gaailiunas <gailiunaslaurynas@tutanota.com>
2025-05-24 17:15:54 +0300

    feat: basic connectivity between server and agent

--- agent/src/main.rs
+++ agent/src/main.rs
@@ -1,3 +1,39 @@
-fn main() {
-    println!("Observus Agent");
+use tokio::io::{AsyncReadExt, AsyncWriteExt};
+use tokio::net::TcpStream;
+use tracing::{Level, debug, error, trace};
+use tracing_subscriber;
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let subscriber = tracing_subscriber::fmt()
+        .compact()
+        .with_file(true)
+        .with_line_number(true)
+        .with_thread_ids(true)
+        .with_target(false)
+        .with_max_level(Level::TRACE)
+        .finish();
+
+    tracing::subscriber::set_global_default(subscriber).expect("setting default subscriber failed");
+
+    let mut stream = TcpStream::connect("127.0.0.1:8080").await?;
+
+    let mut buf = [0; 1024];
+    loop {
+        let n = match stream.read(&mut buf).await {
+            Ok(0) => {
+                debug!("Socket closed");
+                continue;
+            }
+            Ok(n) => {
+                debug!("Read {} bytes", n);
+                trace!("Data: {:?}", &buf[..n]);
+                n
+            }
+            Err(e) => {
+                error!("failed to read from socket; err = {:?}", e);
+                continue;
+            }
+        };
+    }
 }
--- server/src/main.rs
+++ server/src/main.rs
@@ -1,3 +1,45 @@
-fn main() {
-    println!("Observus Server");
+use tokio::io::{AsyncReadExt, AsyncWriteExt};
+use tokio::net::TcpListener;
+use tracing::{Level, debug, error, trace};
+use tracing_subscriber;
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let subscriber = tracing_subscriber::fmt()
+        .compact()
+        .with_file(true)
+        .with_line_number(true)
+        .with_thread_ids(true)
+        .with_target(false)
+        .with_max_level(Level::TRACE)
+        .finish();
+
+    tracing::subscriber::set_global_default(subscriber).expect("setting default subscriber failed");
+
+    let listener = TcpListener::bind("127.0.0.1:8080").await?;
+
+    loop {
+        let (mut socket, _) = listener.accept().await?;
+
+        tokio::spawn(async move {
+            let mut buf = [0; 1024];
+            loop {
+                let n = match socket.read(&mut buf).await {
+                    Ok(0) => {
+                        debug!("Socket closed");
+                        return;
+                    }
+                    Ok(n) => {
+                        debug!("Read {} bytes", n);
+                        trace!("Data: {:?}", &buf[..n]);
+                        n
+                    }
+                    Err(e) => {
+                        error!("failed to read from socket; err = {:?}", e);
+                        return;
+                    }
+                };
+            }
+        });
+    }
 }
